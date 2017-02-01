2908a7afc0fe102893f3165703ac8f9e3b485e81
Nicolas R <cpan@eboxr.com>
2017-02-01 11:21:45 -0700

    adjust tests

--- lib/Amazon/Dash/Button/Device.pm
+++ lib/Amazon/Dash/Button/Device.pm
@@ -5,14 +5,14 @@ use warnings;
 
 use Simple::Accessor qw{mac onClick name timeout last_click _fork_for_onClick};
 
-sub _build_name { ( $_[0]->mac() // '' ) . '- unknown name' }
+sub _build_name { ( $_[0]->mac() // '' ) . ' (unknown name)' }
 sub _build_last_click        { 0 }
 sub _build_timeout           { 5 }
 sub _build__fork_for_onClick { 1 }    # by default fork to run the onClick
 
 sub _build_onClick {
     return sub {
-        print qq{No onClick action for this button: } . $_[0]->name . q{\n};
+        print qq{No onClick action defined for this button: } . $_[0]->name . q{\n};
     };
 }
 
@@ -38,25 +38,28 @@ sub build {
     # check the mac address
     die "mac address is undefined" unless defined $self->mac();
 
-    # check the onClick
-    die "onClick is required" if ref $self->onClick ne 'CODE';
-
     return $self;
+}
+
+sub debug {
+	warn join( ' ', map { $_ // 'undef' } @_ );
 }
 
 sub check {
     my ( $self, $mac2check ) = @_;
 
-    return unless $mac2check;
+    return unless defined $mac2check;
+
+    $mac2check =~ s{:}{}g;
 
     return if $self->mac() ne lc($mac2check);
 
-    warn "Find Button " . $self->name();
+    debug( "Find Button", $self->name() );
     my $now = time();
     return
       if $self->timeout > 0 && ( $now - $self->last_click ) <= $self->timeout;
     $self->last_click($now);
-    warn "perform onClick for " . $self->name();
+    debug( "perform onClick for", $self->name() );
 
     # we want to fork to run the onClick action
     #	we can disable it during unit tests or others
